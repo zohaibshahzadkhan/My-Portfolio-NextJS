a68b88d3b0abe3a9163e33ec90e193a918b77900
Zohaib Shahzad <zohaibshahzad16@gmail.com>
2024-08-26 13:28:06 +0200

    Add experiences page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -12,13 +13,12 @@ export default function Home() {
     mx-auto sm:px-10 px-5 bg-black-100"
     >
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Recommendations />
+        <Experience />
       </div>
     </main>
   );
